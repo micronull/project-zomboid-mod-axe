f5e3f33a32e3358de0c05b0c533203f60f03d2b7
Stepan Ipatyev <istepan@yandex.ru>
2015-09-15 20:26:48 +0500

    add distributions and new poster

---
+++ media/lua/server/Items/DLLootAxes.lua
@@ -0,0 +1,76 @@
+require 'Items/SuburbsDistributions'
+
+-- Zombie Male
+table.insert(SuburbsDistributions["all"]["inventorymale"].items, "AxeCraft.MakeshiftAxe");
+table.insert(SuburbsDistributions["all"]["inventorymale"].items, 0.1);
+table.insert(SuburbsDistributions["all"]["inventorymale"].items, "AxeCraft.WornMakeshiftAxe");
+table.insert(SuburbsDistributions["all"]["inventorymale"].items, 0.1);
+table.insert(SuburbsDistributions["all"]["inventorymale"].items, "AxeCraft.BluntMakeshiftAxe");
+table.insert(SuburbsDistributions["all"]["inventorymale"].items, 0.1);
+-- Zombie Female
+table.insert(SuburbsDistributions["all"]["inventoryfemale"].items, "AxeCraft.MakeshiftAxe");
+table.insert(SuburbsDistributions["all"]["inventoryfemale"].items, 0.1);
+table.insert(SuburbsDistributions["all"]["inventoryfemale"].items, "AxeCraft.WornMakeshiftAxe");
+table.insert(SuburbsDistributions["all"]["inventoryfemale"].items, 0.1);
+table.insert(SuburbsDistributions["all"]["inventoryfemale"].items, "AxeCraft.BluntMakeshiftAxe");
+table.insert(SuburbsDistributions["all"]["inventoryfemale"].items, 0.1);
+
+table.insert(SuburbsDistributions["all"]["crate"].items, "AxeCraft.AxeHandle");
+table.insert(SuburbsDistributions["all"]["crate"].items, 0.8);
+table.insert(SuburbsDistributions["all"]["crate"].items, "AxeCraft.AxeHead");
+table.insert(SuburbsDistributions["all"]["crate"].items, 0.8);
+table.insert(SuburbsDistributions["all"]["crate"].items, "AxeCraft.MakeshiftAxe");
+table.insert(SuburbsDistributions["all"]["crate"].items, 0.8);
+table.insert(SuburbsDistributions["all"]["crate"].items, "AxeCraft.WornAxeHead");
+table.insert(SuburbsDistributions["all"]["crate"].items, 0.8);
+table.insert(SuburbsDistributions["all"]["crate"].items, "AxeCraft.WornMakeshiftAxe");
+table.insert(SuburbsDistributions["all"]["crate"].items, 0.8);
+table.insert(SuburbsDistributions["all"]["crate"].items, "AxeCraft.BluntAxeHead");
+table.insert(SuburbsDistributions["all"]["crate"].items, 0.8);
+table.insert(SuburbsDistributions["all"]["crate"].items, "AxeCraft.BluntMakeshiftAxe");
+table.insert(SuburbsDistributions["all"]["crate"].items, 0.8);
+
+table.insert(SuburbsDistributions["toolstore"]["shelves"].items, "AxeCraft.AxeHandle");
+table.insert(SuburbsDistributions["toolstore"]["shelves"].items, 1);
+table.insert(SuburbsDistributions["toolstore"]["shelves"].items, "AxeCraft.AxeHead");
+table.insert(SuburbsDistributions["toolstore"]["shelves"].items, 1);
+table.insert(SuburbsDistributions["toolstore"]["shelves"].items, "AxeCraft.MakeshiftAxe");
+table.insert(SuburbsDistributions["toolstore"]["shelves"].items, 1);
+table.insert(SuburbsDistributions["toolstore"]["shelves"].items, "AxeCraft.WornAxeHead");
+table.insert(SuburbsDistributions["toolstore"]["shelves"].items, 1);
+table.insert(SuburbsDistributions["toolstore"]["shelves"].items, "AxeCraft.WornMakeshiftAxe");
+table.insert(SuburbsDistributions["toolstore"]["shelves"].items, 1);
+table.insert(SuburbsDistributions["toolstore"]["shelves"].items, "AxeCraft.BluntAxeHead");
+table.insert(SuburbsDistributions["toolstore"]["shelves"].items, 2);
+table.insert(SuburbsDistributions["toolstore"]["shelves"].items, "AxeCraft.BluntMakeshiftAxe");
+table.insert(SuburbsDistributions["toolstore"]["shelves"].items, 2);
+
+table.insert(SuburbsDistributions["gunstore"]["counter"].items, "AxeCraft.AxeHandle");
+table.insert(SuburbsDistributions["gunstore"]["counter"].items, 1);
+table.insert(SuburbsDistributions["gunstore"]["counter"].items, "AxeCraft.AxeHead");
+table.insert(SuburbsDistributions["gunstore"]["counter"].items, 1);
+table.insert(SuburbsDistributions["gunstore"]["counter"].items, "AxeCraft.MakeshiftAxe");
+table.insert(SuburbsDistributions["gunstore"]["counter"].items, 1);
+table.insert(SuburbsDistributions["gunstore"]["counter"].items, "AxeCraft.WornAxeHead");
+table.insert(SuburbsDistributions["gunstore"]["counter"].items, 1);
+table.insert(SuburbsDistributions["gunstore"]["counter"].items, "AxeCraft.WornMakeshiftAxe");
+table.insert(SuburbsDistributions["gunstore"]["counter"].items, 1);
+table.insert(SuburbsDistributions["gunstore"]["counter"].items, "AxeCraft.BluntAxeHead");
+table.insert(SuburbsDistributions["gunstore"]["counter"].items, 1);
+table.insert(SuburbsDistributions["gunstore"]["counter"].items, "AxeCraft.BluntMakeshiftAxe");
+table.insert(SuburbsDistributions["gunstore"]["counter"].items, 1);
+
+table.insert(SuburbsDistributions["gunstorestorage"]["all"].items, "AxeCraft.AxeHandle");
+table.insert(SuburbsDistributions["gunstorestorage"]["all"].items, 1);
+table.insert(SuburbsDistributions["gunstorestorage"]["all"].items, "AxeCraft.AxeHead");
+table.insert(SuburbsDistributions["gunstorestorage"]["all"].items, 1);
+table.insert(SuburbsDistributions["gunstorestorage"]["all"].items, "AxeCraft.MakeshiftAxe");
+table.insert(SuburbsDistributions["gunstorestorage"]["all"].items, 1);
+table.insert(SuburbsDistributions["gunstorestorage"]["all"].items, "AxeCraft.WornAxeHead");
+table.insert(SuburbsDistributions["gunstorestorage"]["all"].items, 1);
+table.insert(SuburbsDistributions["gunstorestorage"]["all"].items, "AxeCraft.WornMakeshiftAxe");
+table.insert(SuburbsDistributions["gunstorestorage"]["all"].items, 1);
+table.insert(SuburbsDistributions["gunstorestorage"]["all"].items, "AxeCraft.BluntAxeHead");
+table.insert(SuburbsDistributions["gunstorestorage"]["all"].items, 1);
+table.insert(SuburbsDistributions["gunstorestorage"]["all"].items, "AxeCraft.BluntMakeshiftAxe");
+table.insert(SuburbsDistributions["gunstorestorage"]["all"].items, 1);
